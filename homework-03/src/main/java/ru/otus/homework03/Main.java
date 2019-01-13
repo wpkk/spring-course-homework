2dbc4f244db1e1d8a165451459aedeb39ef5fef8
George Rylkov <g.rylkov@gmail.com>
2019-01-14 01:26:49 +0300

    Extract the examination application start to ExaminationService

--- homework-03/src/main/java/ru/otus/homework03/Main.java
+++ homework-03/src/main/java/ru/otus/homework03/Main.java
@@ -3,30 +3,15 @@ package ru.otus.homework03;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-import ru.otus.homework03.domain.Question;
-import ru.otus.homework03.domain.Student;
-import ru.otus.homework03.service.EvaluationService;
-import ru.otus.homework03.service.QuestionService;
-import ru.otus.homework03.service.StudentService;
+import ru.otus.homework03.service.ExaminationService;
 
-import java.util.List;
-//todo rename this class
-//@PropertySource("classpath:application.yml")
 @SpringBootApplication()
 public class Main {
 
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(Main.class, args);
-        StudentService studentService = context.getBean(StudentService.class);
-        QuestionService questionService = context.getBean(QuestionService.class);
-        EvaluationService evaluationService = context.getBean(EvaluationService.class);
-
-        Student student = studentService.getStudentInfo();
-        List<Question> questions = questionService.getQuestions();
-        evaluationService.evaluateStudent(student, questionService.askQuestions(questions));
+        context.getBean(ExaminationService.class).startExamination();
 
     }
 
---
+++ homework-03/src/main/java/ru/otus/homework03/service/ExaminationServiceImpl.java
@@ -0,0 +1,32 @@
+package ru.otus.homework03.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.otus.homework03.domain.Question;
+import ru.otus.homework03.domain.Student;
+
+import java.util.List;
+
+@Service
+public class ExaminationServiceImpl implements ExaminationService {
+
+    private final StudentService studentService;
+
+    private final QuestionService questionService;
+
+    private final EvaluationService evaluationService;
+
+    @Autowired
+    public ExaminationServiceImpl(StudentService studentService, QuestionService questionService, EvaluationService evaluationService) {
+        this.studentService = studentService;
+        this.questionService = questionService;
+        this.evaluationService = evaluationService;
+    }
+
+    @Override
+    public void startExamination() {
+        Student student = studentService.getStudentInfo();
+        List<Question> questions = questionService.getQuestions();
+        evaluationService.evaluateStudent(student, questionService.askQuestions(questions));
+    }
+}
