63c90fb4c8e33e73e45c02072b7147ca27f9295c
George <g.rylkov@gmail.com>
2019-07-27 13:55:46 +0300

    Add foreign keys

--- homework-05/src/main/resources/data.sql
+++ homework-05/src/main/resources/data.sql
@@ -1,7 +1,3 @@
-insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title1', 2, 3);
-insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title2', 1, 4);
-insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title3', 1, 2);
-
 insert into authors (ID, NAME, SURNAME, YEAR_BIRTH, YEAR_DEATH) values (default, 'name1', 'author1', '1970', '2000');
 insert into authors (ID, NAME, SURNAME, YEAR_BIRTH, YEAR_DEATH) values (default, 'name2', 'author2', '1973', '2003');
 
@@ -9,3 +5,7 @@ insert into genres (ID, GENRE) values (default, 'genre1');
 insert into genres (ID, GENRE) values (default, 'genre2');
 insert into genres (ID, GENRE) values (default, 'genre3');
 insert into genres (ID, GENRE) values (default, 'genre4');
+
+insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title1', 2, 3);
+insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title2', 1, 4);
+insert into books (ID, TITLE, AUTHOR_ID, GENRE_ID) values (default, 'title3', 1, 2);
