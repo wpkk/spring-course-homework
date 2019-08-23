fa4fb4434c654a67f19fa23bbb72a5dc005cf2ce
wpkk <g.rylkov@gmail.com>
2019-08-23 18:55:04 +0300

    Start adding Liquibase for database initialization

--- homework-06/src/main/java/ru/otus/homework06/Main.java
+++ homework-06/src/main/java/ru/otus/homework06/Main.java
@@ -1,5 +1,6 @@
 package ru.otus.homework06;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +9,9 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-//        Console.main(args);
+        Console.main(args);
 
         SpringApplication.run(Main.class);
 
