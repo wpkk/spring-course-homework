f2595e6ead06894c34b8781c83051f10eedbf593
wpkk <g.rylkov@gmail.com>
2019-09-09 13:09:37 +0300

    Remove the startup of H2 console

--- homework-06/src/main/java/ru/otus/homework06/Main.java
+++ homework-06/src/main/java/ru/otus/homework06/Main.java
@@ -10,11 +10,7 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-
-        Console.main(args);
-
         SpringApplication.run(Main.class);
-
     }
 
 }
